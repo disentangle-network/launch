5be1b433df4124a58f8750a9f085a167d0c22c79
Larsen Close <lclose@quovis.io>
2026-02-21 00:57:08 -0700

    fix: config file perms to 0600, exclude CLI-inherent gosec rules

--- internal/config/config.go
+++ internal/config/config.go
@@ -121,7 +121,7 @@ func Save(cfg *Config, path string) error {
 	if err != nil {
 		return fmt.Errorf("marshaling config: %w", err)
 	}
-	if err := os.WriteFile(path, data, 0o644); err != nil {
+	if err := os.WriteFile(path, data, 0o600); err != nil {
 		return fmt.Errorf("writing config %s: %w", path, err)
 	}
 	return nil
